bcf6d9753f8f9cc6ff1acda07b328890b971a9db
Johums <13276915582@163.com>
2016-03-15 21:28:29 +0800

    支持在本地生成全量的sh文件

--- bigdata/views.py
+++ bigdata/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context
 from django.template.loader import get_template
 
 from utils import conf
@@ -17,9 +19,13 @@ def dataStore(request):
     if request.method == "POST":
         sysid = request.POST["sysid"]
         tableList = [request.POST[tb] for tb in request.POST.keys() if "table" in tb]
-
         tables = map(lambda x: table(x), tableList)
-        template = get_template("odbs_init.tmp")
+
+        save_dir = os.path.join("temp", sysid)
+
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir)
+
         parser = configer.baseparser("conf/db.conf")
         section = parser[conf.DEV_ENV]
         content = {
@@ -30,7 +36,13 @@ def dataStore(request):
             "tables"   : tables
         }
 
+        init_template = get_template("odbs_init.tmp")
+        init_file = os.path.join(save_dir, "i{0}_to_odbs_init.sh".format(sysid))
+        init_content = init_template.render(Context(content))
+        with open(init_file, 'wb') as fi:
+            fi.write(init_content.encode(conf.SH_ENCODING))
+
         response = HttpResponse(content_type="application/x-sh")
         response["Content-Disposition"] = "attachment; filename='i{0}_to_odbs_init.sh'".format(sysid)
-        response.write(template.render(content))
+        response.write(init_content)
     return response
